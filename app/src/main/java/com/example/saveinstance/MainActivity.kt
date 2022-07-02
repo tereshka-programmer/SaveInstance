e62f9760b13ba07cde6017f1d7a493b075bcf336
tereshka-programmer <maksimka.tereschenko@mail.ru>
2022-07-02 19:58:00 +0300

    use Serializable class

--- app/src/main/java/com/example/saveinstance/MainActivity.kt
+++ app/src/main/java/com/example/saveinstance/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.saveinstance
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.core.content.ContextCompat
 import com.example.saveinstance.databinding.ActivityMainBinding
+import java.io.Serializable
 import kotlin.properties.Delegates
 import kotlin.random.Random
 
@@ -12,8 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    private var counterValue by Delegates.notNull<Int>()
-    private var texttColor by Delegates.notNull<Int>()
+    lateinit var state: State
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,29 +24,29 @@ class MainActivity : AppCompatActivity() {
         binding.btIncrement.setOnClickListener { increment() }
         binding.btRandomColor.setOnClickListener { setRandomColor() }
 
-        if (savedInstanceState == null){
-            counterValue = 5
-            texttColor = ContextCompat.getColor(this, R.color.purple_700)
+        state = if (savedInstanceState == null){
+            State(
+                counterValue = 0,
+                texttColor = ContextCompat.getColor(this, R.color.purple_700)
+            )
         } else {
-            counterValue = savedInstanceState.getInt(KEY_COUNTER)
-            texttColor = savedInstanceState.getInt(KEY_COLOR)
+            savedInstanceState.getSerializable(KEY_STATE) as State
         }
         renderState()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt(KEY_COUNTER, counterValue)
-        outState.putInt(KEY_COLOR, texttColor)
+        outState.putSerializable(KEY_STATE, state)
     }
 
     private fun increment(){
-        counterValue++
+        state.counterValue++
         renderState()
     }
 
     private fun setRandomColor(){
-        texttColor = Color.rgb(
+        state.texttColor = Color.rgb(
             Random.nextInt(256),
             Random.nextInt(256),
             Random.nextInt(256)
@@ -54,12 +55,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun renderState(){
-        binding.tvCounter.text = counterValue.toString()
-        binding.tvCounter.setTextColor(texttColor)
+        binding.tvCounter.text = state.counterValue.toString()
+        binding.tvCounter.setTextColor(state.texttColor)
     }
 
+
+    class State(
+        var counterValue: Int,
+        var texttColor: Int
+    ) : Serializable
+
     companion object{
-        const val KEY_COUNTER = "KEY_COUNTER"
-        const val KEY_COLOR = "KEY_COLOR"
+        const val KEY_STATE = "STATE"
     }
 }
